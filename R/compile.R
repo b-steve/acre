a514947ffc39bf146ce3d78ecba60209122b4db8
Ben Stevenson <ben.stevenson@auckland.ac.nz>
2023-05-04 14:54:51 +1200

    Changing the old package name in some places.

--- R/compile.R
+++ R/compile.R
@@ -1,21 +1,21 @@
 #' @export
-compile.ascr <- function(dev = FALSE, debug.mode = FALSE){
+compile.acre <- function(dev = FALSE, debug.mode = FALSE){
   if(dev){
     dir = paste0(getwd(), '/inst/TMB/')
   } else {
-    dir = paste0(system.file(package = "ascr"), "/TMB/")
+    dir = paste0(system.file(package = "acre"), "/TMB/")
   }
   
-  if(file.exists(paste0(dir, "ascrTmb.o"))) unlink(paste0(dir, "ascrTmb.o"))
-  if(file.exists(paste0(dir, "ascrTmb.dll"))) unlink(paste0(dir, 'ascrTmb.dll'))
+  if(file.exists(paste0(dir, "acreTmb.o"))) unlink(paste0(dir, "acreTmb.o"))
+  if(file.exists(paste0(dir, "acreTmb.dll"))) unlink(paste0(dir, 'acreTmb.dll'))
 
   if(!debug.mode){
-    TMB::compile(paste0(dir, "ascrTmb.cpp"), framework = "CppAD")
+    TMB::compile(paste0(dir, "acreTmb.cpp"), framework = "CppAD")
   } else {
     if(Sys.info()['sysname'] == 'Windows'){
-      TMB::compile(paste0(dir, "ascrTmb.cpp"), "-O1 -g", DLLFLAGS="")
+      TMB::compile(paste0(dir, "acreTmb.cpp"), "-O1 -g", DLLFLAGS="")
     } else {
-      TMB::compile(paste0(dir, "ascrTmb.cpp"), "-O0 -g")
+      TMB::compile(paste0(dir, "acreTmb.cpp"), "-O0 -g")
     }
     
   }
