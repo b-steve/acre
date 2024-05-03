34ca1af845ce260aea4547f31794995d60d2d467
Ben Stevenson <ben.stevenson@auckland.ac.nz>
2024-05-03 11:46:27 +0100

    Some DESCRIPTION and NAMESPACE stuff for R CMD check.

--- R/acre.R
+++ R/acre.R
@@ -1,6 +1,8 @@
 #' @useDynLib acre
 NULL
 
+#' @import Rcpp testthat
+
 #' @importFrom ggplot2 ggplot aes geom_point geom_segment geom_path theme_void coord_quickmap scale_shape_identity labs arrow unit ggtitle guides guide_colourbar
 NULL
 
