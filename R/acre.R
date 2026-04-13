53efed6adc5fb8215b0b0d23e3d2e4025da03e56
Ben Stevenson <ben.stevenson@auckland.ac.nz>
2026-04-14 11:23:27 +1200

    Tidying up @import statements a little.

--- R/acre.R
+++ R/acre.R
@@ -2,12 +2,7 @@
 NULL
 
 #' @import Rcpp testthat
-
 #' @importFrom stats aggregate coef confint cor dist median pnorm qnorm quantile rgamma rnorm rpois runif sd sigma vcov
-NULL
-
 #' @importFrom methods is
-NULL
-
 #' @importFrom graphics abline axis box contour hist image legend lines par plot.new plot.window points title
 NULL
